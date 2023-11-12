bcb6f8d73a587529cba92db49690d91d93192a58
tom56561 <tom56561@gmail.com>
2023-11-12 14:54:56 -0500

    feat: add kanbas 4.2

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import Hello from './hello.js';
 import Lab5 from './Lab5.js';
+import CourseRoutes from "./courses/routes.js";
 import cors from "cors";
 const app = express();
 app.use(express.json());
 app.use(cors());
 Hello(app);
 Lab5(app)
+CourseRoutes(app)
 app.listen(4000);
