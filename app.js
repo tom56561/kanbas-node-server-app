b36df44d399fd95b5179a756563260ef73b84b93
tom56561 <tom56561@gmail.com>
2023-11-12 12:47:58 -0500

    feat: add lab 3.4

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import Hello from './hello.js';
 import Lab5 from './Lab5.js';
+import cors from "cors";
 const app = express();
 app.use(express.json());
+app.use(cors());
 Hello(app);
 Lab5(app)
 app.listen(4000);
