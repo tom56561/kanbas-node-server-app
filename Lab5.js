86f568be94b9a2e6687a022096ef7ffef35669ca
tom56561 <tom56561@gmail.com>
2023-11-12 13:48:38 -0500

    feat: add lab 3.5

--- Lab5.js
+++ Lab5.js
@@ -33,17 +33,58 @@ const Lab5 = (app) => {
             res.status(404).send('Todo not found');
         }
     });
+    app.put("/a5/todos/:id", (req, res) => {
+        const { id } = req.params;
+        const todo = todos.find((t) => t.id === parseInt(id));
+        if (!todo) {
+            res.res
+                .status(404)
+                .json({
+                    message:
+                        `Unable to update Todo with ID ${id}`
+                });
+            return;
+        }
+        todo.title = req.body.title;
+        todo.description = req.body.description;
+        todo.due = req.body.due;
+        todo.completed = req.body.completed;
+        res.sendStatus(200);
+    });
     app.get("/a5/todos/:id/title/:title", (req, res) => {
         const { id, title } = req.params;
         const todo = todos.find((t) => t.id === parseInt(id));
         todo.title = title;
         res.json(todos);
     });
+    app.delete("/a5/todos/:id", (req, res) => {
+        const { id } = req.params;
+        const todo = todos.find((t) => t.id === parseInt(id));
+        if (!todo) {
+            res.res
+                .status(404)
+                .json({
+                    message:
+                        `Unable to delete Todo with ID ${id}`
+                });
+            return;
+        }
+        todos.splice(todos.indexOf(todo), 1);
+        res.sendStatus(200);
+    });
     app.get("/a5/todos/:id/delete", (req, res) => {
         const { id } = req.params;
         const todo = todos.find((t) => t.id === parseInt(id));
         todos.splice(todos.indexOf(todo), 1);
         res.json(todos);
+    });
+    app.post("/a5/todos", (req, res) => {
+        const newTodo = {
+            ...req.body,
+            id: new Date().getTime(),
+        };
+        todos.push(newTodo);
+        res.json(newTodo);
     });
     app.get("/a5/todos/create", (req, res) => {
         const newTodo = {
