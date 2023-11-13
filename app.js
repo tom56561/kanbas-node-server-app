946a38b52f16d13883280f9a4d54e68d1365f26b
tom56561 <tom56561@gmail.com>
2023-11-13 15:02:51 -0500

    feat: add kanbas 4.3.5 Extra Credit

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ import Hello from './hello.js';
 import Lab5 from './Lab5.js';
 import CourseRoutes from './courses/routes.js';
 import ModuleRoutes from './modules/routes.js';
+import AssignmentRoutes from './assignments/routes.js';
 import cors from "cors";
 const app = express();
 app.use(express.json());
 app.use(cors());
-ModuleRoutes(app);
 Hello(app);
-Lab5(app)
-CourseRoutes(app)
+Lab5(app);
+CourseRoutes(app);
+ModuleRoutes(app);
+AssignmentRoutes(app);
 app.listen(4000);
