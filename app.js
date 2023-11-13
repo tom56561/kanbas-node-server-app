6c7f7f80781672c3292670f9c963f59dab8d87a8
tom56561 <tom56561@gmail.com>
2023-11-13 16:39:05 -0500

    config: setting PORT

--- app.js
+++ app.js
@@ -5,12 +5,18 @@ import CourseRoutes from './courses/routes.js';
 import ModuleRoutes from './modules/routes.js';
 import AssignmentRoutes from './assignments/routes.js';
 import cors from "cors";
+import "dotenv/config";
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    })
+);
 Hello(app);
 Lab5(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
