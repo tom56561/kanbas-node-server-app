27033cb03d8cd2c7fdbf26663cad7574db960b06
tom56561 <tom56561@gmail.com>
2023-12-02 16:32:25 -0500

    setting: set mongoDB server

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ import "dotenv/config";
 import mongoose from 'mongoose';
 import UserRoutes from "./users/routes.js";
 
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+console.log(CONNECTION_STRING)
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(express.json());
 app.use(
